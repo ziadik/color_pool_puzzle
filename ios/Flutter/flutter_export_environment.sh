898b37bf523d7adb67b2bf2ec17a9e0f3fb5c8bf
dimitrii <dmitry.zyadik@gmail.com>
2026-03-27 11:09:40 +0500

    0327 1109 del dev dialog

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/dmitriiziadik/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/dmitriiziadik/Projects/flutter/color_pool_puzzle"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Projects\flutter\color_pool_puzzle"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1.0.0"
